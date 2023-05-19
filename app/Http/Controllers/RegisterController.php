c11660bf238ce50786fc695f8f7a559667671c46
Fran <fran69s@hotmail.es>
2023-05-19 13:05:23 +0200

    Validación y creación de usuarios

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index() {
+        return view('auth.register');
+    }
+
+    public function store(Request $registro) {
+        $registro->request->add(['username' => Str::slug($registro->username)]);
+
+        $this->validate($registro, [
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        User::create([
+            'name' => $registro->name,
+            'username' => $registro->username,
+            'email' => $registro->email,
+            'password' => Hash::make($registro->password)
+        ]);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <title>Devstagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
     </head>
-    <body>
+    <body class="bg-gray-100">
         <header class="p-5 border-b bg-white  shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">
@@ -15,7 +15,7 @@
 
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
                 </nav>
             </div>
         </header>
@@ -27,7 +27,7 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
             DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>
         
