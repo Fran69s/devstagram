3342af4e7c9ec3b48937033d2a4b4e4b22c154e8
Fran <fran69s@hotmail.es>
2023-05-23 13:12:37 +0200

    Añadida seguridad al logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,14 @@
                                 {{ auth()->user()->username }}
                             </span>
                         </a>
-                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">Cerrar Sesión</a>
+
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">
+                                Cerrar Sesión
+                            </button>
+                        </form>
+
                     </nav>
                 @endauth
 
